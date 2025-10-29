bd868e83c3449165a8151879efdaf8b1af88ddcd
Arubato111 <albert_katana@gmx.de>
2025-10-29 18:56:03 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./app/**/*.{ts,tsx,js,jsx,mdx}"],
   theme: { extend: {} },
-  plugins: [],
+  plugins: []
 };
